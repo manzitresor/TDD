27228a984092e61aee1a3b04eb31e0df6a8e30ca
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-07 14:48:18 +0200

    finish factorial test and factorial method

--- solver.rb
+++ solver.rb
@@ -1,6 +1,8 @@
 class Solver
   def factorial(number)
+    return 'Input should be a positive number!' if number.negative?
     return 1 if number < 1
+
     number * factorial(number - 1)
   end
 end
