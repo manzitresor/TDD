647c8da6f3400d2ca40749c16b7e1e52719c6e8c
koppai <psthu.koppai@gmail.com>
2023-09-07 19:22:01 +0630

    Add test for reverse method

--- solver.rb
+++ solver.rb
@@ -1,6 +1,6 @@
 class Solver
   def factorial(number)
-    return 'Input should be a positive number!' if number.negative?
+    return "Input should be a positive number!" if number.negative?
     return 1 if number < 1
 
     number * factorial(number - 1)
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,24 +1,31 @@
-require_relative '../solver'
+require_relative "../solver"
 
 describe Solver do
-  describe '#factorial' do
-    context 'when input is greater than 0 or 1' do
-      it 'should return factorial greater than 1' do
+  describe "#factorial" do
+    context "when input is greater than 0 or 1" do
+      it "should return factorial greater than 1" do
         expect(subject.factorial(5)).to eq 120
       end
     end
 
-    context 'when input is 0 or 1' do
-      it 'should return factorial 1' do
+    context "when input is 0 or 1" do
+      it "should return factorial 1" do
         expect(subject.factorial(0) && subject.factorial(1)).to eq 1
       end
     end
 
-    context 'when input is negative' do
-      it 'should print error message' do
-        expect(subject.factorial(-1)).to eq 'Input should be a positive number!'
+    context "when input is negative" do
+      it "should print error message" do
+        expect(subject.factorial(-1)).to eq "Input should be a positive number!"
       end
     end
   end
+
+  describe '#reverse' do
+    context 'When input string' do
+      it 'should return a reversed string' do
+        expect(subject.reverse('abcd')).to eql 'dcba'
+      end
+    end
   end
 end
